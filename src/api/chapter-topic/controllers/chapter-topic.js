13b6252bd0055451952ef221ff24e9b75ac90094
waqar sheikh <waqarsheikh895@gmail.com>
2022-04-07 16:07:34 +0500

    chapterwise learning table added

---
+++ src/api/chapter-topic/controllers/chapter-topic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  chapter-topic controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::chapter-topic.chapter-topic');
---
+++ src/api/chapter-topic/routes/chapter-topic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * chapter-topic router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::chapter-topic.chapter-topic');
