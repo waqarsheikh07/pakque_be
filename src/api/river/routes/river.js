22586326e0ad3236f22ba5048d6ad37768036f02
waqar sheikh <waqarsheikh895@gmail.com>
2022-06-11 11:55:46 +0500

    add rvier and quies collection

---
+++ src/api/river/routes/river.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * river router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::river.river');
---
+++ src/api/river/services/river.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * river service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::river.river');
